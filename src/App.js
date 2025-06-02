8d68102452bd0808b46aa1cbf71f049b710e0a9d
zakaria <zakariabouaicha20051973@gmail.com>
2025-06-02 10:40:57 +0100

    add

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import { CssBaseline } from '@mui/material';
 
 function App() {
   return (
     <>
 
-    <Router>
-
-      <CssBaseline />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
+     
     </>
   );
 }
