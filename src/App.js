80c1bff3fb8c2a09a9b248a9818e41f56d889916
zakaria <zakariabouaicha20051973@gmail.com>
2025-06-02 21:28:29 +0100

    أضفت صفحة لإضافة فيلم وربطتها بفايرستور مع تنفيذ وظيفتي الإضافة والحذف

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-// src/App.js
+
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import AddMovie from './pages/AddMovie';
+import { getFirestore,collection, } from 'firebase/firestore';
 
-function App() {
-  return (
-    <>
 
-   <h1>hello </h1>
-    </>
-  );
+export default function App() {
+return (
+<Router>
+<Routes>
+<Route path="/" element={<Home />} />
+<Route path="/add" element={<AddMovie />} />
+</Routes>
+</Router>
+);
 }
-
-export default App;
